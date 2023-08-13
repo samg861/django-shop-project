514edd3c1311db45d6ba9dea0dea908430595a4e
samg861 <16475396+samg861@users.noreply.github.com>
2023-08-13 18:41:32 +0100

    add more tests (#6)
    * add more tests
    
    * fix linting

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .views import SignupPageView
+from .forms import CustomUserCreationForm
 
 
 class CustomUserTests(TestCase):
@@ -38,3 +41,12 @@ class SignUpPageTests(TestCase):
         self.assertEqual(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, "registration/signup.html")
         self.assertContains(self.response, "Sign Up")
+
+    def test_signup_form(self):
+        form = self.response.context.get("form")
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, "csrfmiddlewaretoken")
+
+    def test_signup_view(self):
+        view = resolve("/accounts/signup/")
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
