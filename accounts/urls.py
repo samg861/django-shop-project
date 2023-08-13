356d53007d5855185447bd050234448efc4b6fda
samg861 <16475396+samg861@users.noreply.github.com>
2023-08-13 20:08:54 +0100

    implement black, and add isort and black to github actions workflow

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-app_name = 'accounts'
+app_name = "accounts"
 
 urlpatterns = [
-    path('signup/', views.SignupPageView.as_view(), name='signup'),
+    path("signup/", views.SignupPageView.as_view(), name="signup"),
 ]
