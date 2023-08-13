607a77eaa52df94423b08c7751819d0d1df2bf3f
samg861 <16475396+samg861@users.noreply.github.com>
2023-08-13 18:37:29 +0100

    add more tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
 
+from .views import SignupPageView
+from .forms import CustomUserCreationForm
 
 class CustomUserTests(TestCase):
     def test_create_user(self):
@@ -38,3 +40,12 @@ class SignUpPageTests(TestCase):
         self.assertEqual(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, "registration/signup.html")
         self.assertContains(self.response, "Sign Up")
+
+    def test_signup_form(self):
+        form = self.response.context.get("form")
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, "csrfmiddlewaretoken")
+
+    def test_signup_view(self):
+        view = resolve("/accounts/signup/")
+        self.assertEqual(view.func.__name__, SignupPageView.as_view().__name__)
