8803539d84c87833d7b0dbe9f80bf067312a4345
samg861 <16475396+samg861@users.noreply.github.com>
2023-08-18 21:37:39 +0100

    add bs5, about page, crispy forms

--- pages/tests.py
+++ pages/tests.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import resolve, reverse
 
-from .views import HomePageView
+from .views import AboutPageView, HomePageView
 
 
 class HomepageTests(SimpleTestCase):
@@ -18,3 +18,20 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_url_resolves_homepageview(self):
         view = resolve("/")
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("pages:about")
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, "about.html")
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve("/about/")
+
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)
