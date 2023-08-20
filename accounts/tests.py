472b486fcc3ea147d05fe57991db2b9fcc17b888
samg861 <16475396+samg861@users.noreply.github.com>
2023-08-20 19:04:09 +0100

    fix linting

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import resolve, reverse
-
-from .forms import CustomUserCreationForm
+from django.urls import reverse
 
 
 class CustomUserTests(TestCase):
@@ -46,7 +44,7 @@ class SignupPageTests(TestCase):
         self.assertNotContains(self.response, "Hi there! I should not be on the page.")
 
     def test_signup_form(self):
-        new_user = get_user_model().objects.create_user(self.username, self.email)
+        _ = get_user_model().objects.create_user(self.username, self.email)
         self.assertEqual(get_user_model().objects.all().count(), 1)
         self.assertEqual(get_user_model().objects.all()[0].username, self.username)
         self.assertEqual(get_user_model().objects.all()[0].email, self.email)
