19ecd30ebac306db4ddcae7759c02a2591cf9099
samg861 <16475396+samg861@users.noreply.github.com>
2023-09-02 16:15:05 +0100

    fix isort

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
 from django.views.generic import DetailView, ListView
 
 from .models import Book
